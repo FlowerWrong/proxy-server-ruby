ccefa182a8ab0ce73c494f36dd629556a9387e46
flowerwrong <sysuyangkang@gmail.com>
2015-07-24 11:48:40 +0800

    Refactor for multi server

--- config/deploy.example.rb
+++ config/deploy.example.rb
@@ -4,24 +4,30 @@ require 'active_support/core_ext/hash/keys'
 PROXY_CONFIG = YAML.load(File.open(File.dirname(__FILE__) + '/setting.yml')).symbolize_keys
 default_config = PROXY_CONFIG[:proxy].symbolize_keys
 
+domains = default_config[:hosts].split(' ')
+deploy_paths = default_config[:deploy_path].split(' ')
+rvm_paths = default_config[:rvm_path].split(' ')
+ssh_users = default_config[:ssh_user].split(' ')
+rvm_defaults = default_config[:rvm_default].split(' ')
+
 require 'mina/bundler'
 require 'mina/git'
 require 'mina/rvm'
 
-set :domain, default_config[:hosts].split(' ')[0]
-set :deploy_to, default_config[:deploy_path]
+set :domain, domains[0]
+set :deploy_to, deploy_paths[0]
 set :repository, 'https://github.com/FlowerWrong/proxy-server-ruby.git'
 set :branch, 'master'
 
 # For system-wide RVM install.
-set :rvm_path, default_config[:rvm_path]
+set :rvm_path, rvm_paths[0]
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['log/proxy_server.log']
 
 # Optional settings:
-set :user, default_config[:ssh_user]    # Username in the server to SSH to.
+set :user, ssh_users[0]    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
@@ -29,7 +35,7 @@ set :user, default_config[:ssh_user]    # Username in the server to SSH to.
 # `mina deploy` or `mina rake`.
 task :environment do
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke default_config[:rvm_default].to_sym
+  invoke rvm_defaults[0].to_sym
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -66,13 +72,19 @@ end
 
 
 # https://github.com/mina-deploy/mina/issues/8
-set :domains, default_config[:hosts].split(' ')
+set :domains, domains
 
 desc "Setup to all servers"
 task :setup_all do
   isolate do
-    domains.each do |domain|
+    domains.each_with_index do |domain, index|
       set :domain, domain
+      set :deploy_to, deploy_paths[index]
+      set :rvm_path, rvm_paths[index]
+      set :user, ssh_users[index]
+      task :environment do
+        invoke rvm_defaults[index].to_sym
+      end
       invoke :setup
       run!
     end
